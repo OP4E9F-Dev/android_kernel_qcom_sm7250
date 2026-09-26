bd1f1b6f6900d4a9b09ba9d0621ddb71484f776b
adbzxcghf <adbzxcghf@outlook.com>
2026-09-26 22:52:44 +0800

    treewide: oplus: Address clang-19 build warnings

--- drivers/power/oplus/charger_ic/oplus_battery_msm7250_R.c
+++ drivers/power/oplus/charger_ic/oplus_battery_msm7250_R.c
@@ -9191,7 +9191,7 @@ bool oplus_chg_check_qchv_condition(void)
 	return false;
 }
 
-int oplus_chg_set_qc_config()
+int oplus_chg_set_qc_config(void)
 {
 	int ret = -1;
 	struct smb_charger *chg = NULL;
@@ -10496,7 +10496,7 @@ void oplus_get_usbtemp_volt(struct oplus_chg_chip *chip)
 	return ;
 }
 
-void oplus_set_typec_sinkonly()
+void oplus_set_typec_sinkonly(void)
 {
 	int rc;
 	struct smb_charger *chg = NULL;
